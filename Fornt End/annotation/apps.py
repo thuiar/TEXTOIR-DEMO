3526559e029930b0a9c150fa732da79b4688015a
lixiaoteng <37832030+XTenLee@users.noreply.github.com>
2021-02-28 16:47:20 +0800

    Add files via upload
    Front End of TEXTOIR

---
+++ Fornt End/annotation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AnnotationConfig(AppConfig):
+    name = 'annotation'
---
+++ Fornt End/detection/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DetectionConfig(AppConfig):
+    name = 'detection'
